2a86a16f0acaedfd8e299da92ed3f87ee25b869c
Christian R <christianriis.kodehode@gmail.com>
2023-01-19 21:47:47 +0100

    removed-comments-js

--- script.js
+++ script.js
@@ -2,32 +2,24 @@ let moodChecker = true;
 
 function moodChanger() {
   if (moodChecker === true) {
-    // CHANGE BUTTON TO ---SUN---
     let btnImg = document.getElementById("moodChangerImg");
     btnImg.src = "./project-files/images/icon-moon.svg";
 
-    // CHANGE TOP BACKGROUND IMAGE
     let topImg = document.getElementById("top-img");
     topImg.src = "./project-files/images/bg-desktop-light.jpg";
 
-    // SET BODY CLASSNAME TO NONE
     document.body.className = "";
 
-    // SWITCH MOODCHECKER VARIABLE TO ---FALSE---
     moodChecker = false;
   } else if (moodChecker === false) {
-    // CHANGE BUTTON TO ---MOON---
     let btnImg = document.getElementById("moodChangerImg");
     btnImg.src = "./project-files/images/icon-sun.svg";
 
-    // CHANGE TOP BACKGROUND IMAGE
     let topImg = document.getElementById("top-img");
     topImg.src = "./project-files/images/bg-desktop-dark.jpg";
 
-    // SET BODY CLASSNAME TO ---DARK---
     document.body.className = "dark";
 
-    // SWITCH MOODCHECKER TO ---TRUE---
     moodChecker = true;
   }
 }
